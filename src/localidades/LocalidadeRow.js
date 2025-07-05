f1a35b087baffeed15d6b460d16dda33d0aec084
Gabriel Laurindo <gabriel.laurindo01@outlook.com.br>
2025-07-05 19:58:06 -0300

    Primeiro commit do frontend

---
+++ src/localidades/LocalidadeRow.js
@@ -0,0 +1,47 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+const DataRow = styled.tr`
+  &:hover {
+    background: rgba(0,0,0,0);
+	  color: darkgrey;
+    cursor: pointer;
+	  box-shadow: inset 0 0 0 2px darkgrey;
+  }
+  &:nth-child(even) {
+    background: lightgray;
+  }
+`;
+
+const DataCell = styled.td`
+  padding-top: 12px;
+  padding-bottom: 12px;
+`;
+
+const DescricaoCell = styled(DataCell)`
+  padding-left: 12px;
+`;
+
+const EstadoCell = styled(DataCell)`
+  padding-left: 12px;
+`;
+
+function LocalidadeRow(props) {
+  const { localidade } = props;
+  const navigate = useNavigate();
+
+  const selectLocalidade = () => {
+    navigate(`/localidades/${localidade.id}`);
+  }
+
+  return (
+    <DataRow onClick={selectLocalidade}>
+      <DescricaoCell>{localidade.descricao}</DescricaoCell>
+      <EstadoCell>{localidade.estado}</EstadoCell>
+    </DataRow>
+  );
+}
+
+
+export default LocalidadeRow;
